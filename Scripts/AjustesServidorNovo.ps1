be2ebe7a471d7ee5f4fc895687bd68862919d893
kimagliardi <kim.agliardi@gmail.com>
2017-05-05 16:01:25 -0300

    Adicionando scripts
    "Ajuste de servidores e movimentação de usuários"

---
+++ Scripts/AjustesServidorNovo.ps1
@@ -0,0 +1,54 @@
+$computer = Read-Host "Informe o nome do computador: "
+
+RENAME-COMPUTER -computername $env:COMPUTERNAME -NewName $computer
+
+## Desabilitando opcões do IPv6 | Habilitando Ping
+netsh int ipv6 isatap set state disabled
+netsh int ipv6 6to4 set state disabled
+netsh interface teredo set state disable
+
+# Opcões de Firewall
+Import-Module NetSecurity
+New-NetFirewallRule -Name Allow_Ping -DisplayName “Allow Ping”  -Description “Packet Internet Groper ICMPv4” -Protocol ICMPv4 -IcmpType 8 -Enabled True -Profile Any -Action Allow
+
+## Desabilitando opcões na placa de rede
+Disable-NetAdapterBinding -Name "Ethernet0" -ComponentID ms_rspndr
+Disable-NetAdapterBinding -Name "Ethernet0" -ComponentID ms_lltdio
+Disable-NetAdapterBinding -Name "Ethernet0" -ComponentID ms_implat
+Disable-NetAdapterBinding -Name "Ethernet0" -ComponentID ms_pacer
+Disable-NetAdapterBinding -Name "Ethernet0" -ComponentID ms_tcpip6
+
+#Ajusta IP, mascara, gateway e DNS da estação (dom. "aneas.local")
+$ipv4 = Read-Host "Informe o endereco IP do computador: "
+$gateway = Read-Host "Informe o endereco do Gateway"
+New-NetIPAddress -InterfaceAlias "Ethernet0" -IPAddress $ipv4 -PrefixLength 24 -DefaultGateway $gateway
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddresses 200.188.171.3, 200.188.170.3
+
+# Desabilita opção "Enable LMHOSTS Lookup"
+$nic = [wmiclass]'Win32_NetworkAdapterConfiguration'
+$nic.enablewins($false,$false)
+
+#Ajusta as configurações de netbios para "Enable NetBIOS over TCP/IP"
+$nic = gwmi 'Win32_NetworkAdapterConfiguration'
+$nic.settcpipnetbios(1)
+
+#anexa sufixos de DNS
+New-ItemProperty -Path HKLM:\System\CurrentControlSet\Services\TCPIP\Parameters\ -Name SearchList -PropertyType String -Value "aneas.org.br,aneas.local" -Force
+## Fim das opcões de rede
+
+
+# Enable Remote Desktop
+(Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1,1) | Out-Null
+(Get-WmiObject -Class "Win32_TSGeneralSetting" -Namespace root\cimv2\TerminalServices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0) | Out-Null
+Get-NetFirewallRule -DisplayName "Remote Desktop*" | Set-NetFirewallRule -enabled true
+
+## Ativação do Windows 2012 R2 std - Serial ASAV
+$key = "inserir a chave do Windows"
+$service = get-wmiObject -query “select * from SoftwareLicensingService”
+$service.InstallProductKey($key)
+$service.RefreshLicenseStatus()
+## Fim do bloco de ativação do Windows
+
+## Desabilita a exibição do server manager no startup
+New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" –Force
+## 
---
+++ Scripts/Move_AD_users_from_CSV_File.ps1
@@ -0,0 +1,31 @@
+﻿
+#################################################################
+# This script will help yo move bulk ad accounts into target OU
+# Written 08/08/15 Casey, Dedeal
+# Fell free to change use any part of this script
+# http://www.smtp25.blogspot.com/
+#################################################################
+# Import AD Module
+import-module ActiveDirectory
+
+# Import CSV 
+$MoveList = Import-Csv -Path "C:\Temp\Acc_MoveList.csv"
+# Specify target OU.This is where users will be moved.
+$TargetOU =  "OU=SVC_Users,OU=VA,DC=TekPros,DC=com"
+# Import the data from CSV file and assign it to variable 
+$Imported_csv = Import-Csv -Path "C:\temp\Acc_MoveList.csv" 
+
+$Imported_csv | ForEach-Object {
+     # Retrieve DN of User.
+     $UserDN  = (Get-ADUser -Identity $_.Name).distinguishedName
+     Write-Host " Moving Accounts ..... "
+     # Move user to target OU.
+     Move-ADObject  -Identity $UserDN  -TargetPath $TargetOU
+     
+ }
+ Write-Host " Completed move " 
+ $total = ($MoveList).count
+ $total
+ Write-Host "Accounts have been moved succesfully..."
+ 
+ 
