f48efd8bb086b2d3bab5b15c779f883fedab7062
kimagliardi <kim.agliardi@gmail.com>
2018-03-09 13:12:59 -0300

    Update WindowsUpdateFromList.ps1

--- Scripts/SCCM/WindowsUpdateFromList.ps1
+++ Scripts/SCCM/WindowsUpdateFromList.ps1
@@ -1,9 +1,9 @@
-$csv = import-csv C:\temp\updates_ariel.csv -Delimiter ","
+$csv = import-csv C:\temp\updates.csv -Delimiter ","
 $Groups = Get-PSWSUSGroup -Name 'computadores'
 
 
 Import-Module PoshWSUS
-Connect-PSWSUSServer -WsusServer med-wsus.colegiomedianeira.g12.br -Port 8530
+Connect-PSWSUSServer -WsusServer <servername> -Port 8530
 
 foreach($line in $csv){
     #Write-Host $line.update
