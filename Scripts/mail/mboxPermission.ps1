ec89ce3f5eaf671168aba0747f368b6fb32bda07
kimagliardi <kim.agliardi@gmail.com>
2018-04-18 15:49:55 -0300

    update sharedmailbox script
    Putting all sharedmailboxes on the same line on CSV file..

--- Scripts/mail/mboxPermission.ps1
+++ Scripts/mail/mboxPermission.ps1
@@ -1,4 +1,14 @@
-﻿$csv = Import-csv C:\anchieta\sharedAnchieta.csv -Delimiter ";"
+param (
+    [Parameter(Mandatory=$true)][string]$userfile
+)
+
+$csv = Import-csv $userfile -Delimiter ";"
+
 foreach($line in $csv){
-    Add-MailboxPermission -Identity $line.Identity -AccessRights FullAccess -InheritanceType All -AutoMapping:$true -User $line.User
+    #spliting identity line
+    $id = $line.identity.Split(";")
+    for($i=0; $i -lt $id.Length; $i++){
+        Add-MailboxPermission -Identity $id[$i] -AccessRights FullAccess -InheritanceType All -AutoMapping:$true -User $line.User
+        Write-Host "Adicionando permissão de caixa de correio em: " $id[$i] "para o usuário: " $line.user
+    }
 }
